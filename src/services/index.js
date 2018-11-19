88212924d97aa44b8710ea39b463a8a839bae99a
Ward Delabastita <ward.delabastita@tenforce.com>
2018-11-19 13:55:44 +0100

    add applications service based on mongoose ORM

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,5 @@
+const applications = require("./applications/applications.service.js");
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
+  app.configure(applications);
 };
