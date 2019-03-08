ad91108fbfc0b17979c888c702f955ebb9495034
Arnaud S'Jongers <arnaud.sjongers@tenforce.com>
2019-03-08 17:46:45 +0100

    Remove duplicate line

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,6 @@
 const applications = require("./applications/applications.service.js")
 const policies = require("./policies/policies.service.js")
 const users = require("./users/users.service.js")
-const users = require("./users/users.service.js")
 
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
