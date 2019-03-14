6163336c0e8c91c1337a198b4292971bc2fff8f9
Ward Delabastit <ward.delabastita@tenforce.com>
2019-03-14 09:23:05 +0100

    add logic that was lost during a previous merge

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,20 +1,13 @@
-const request = require("request-promise-native")
-const session = require("express-session")
 
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
     // Add your custom middleware here. Remember that
     // in Express, the order matters.
-    
-    app.use(session({
-        resave: false,
-        saveUninitialized: false,
-    }))
 
     app.use((req, res, next) => {
         // extract userObject from header
         // and make it available for subsequent logic
-        if (request.headers) {
+        if (req.headers) {
             req.feathers.userObject = getUserFromRequest(req)
         }
 
--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -1,4 +1,5 @@
 const logger = require("../../logger")
+const arraysAreEqual = require("../../util/arrays-are-equal")
 
 module.exports = {
     before: {
@@ -10,9 +11,10 @@ module.exports = {
                 // logger.info("before get user")
                 // logger.info(JSON.stringify(context))
                 
-                const userId = context.params.userId
-                if (userId === "current") {
+                if (context.id === "current") {
                     const userObject = context.params.userObject
+                    context.id = userObject.id
+                    
                     const usersService = context.service
                     
                     const result = await usersService.find({ query: { id: userObject.id }})
@@ -28,30 +30,101 @@ module.exports = {
                         // user already existed
                         // update info just received
                         // logger.info("auth user update")
-                        await service.patch(result.users[0]._id, { ...userObject })
+                        await usersService.patch(result.users[0]._id, { ...userObject })
                         // logger.info("after patch")
                         // logger.info(JSON.stringify(result))
                     } else {
-                        logger.info(`more than 1 user found with the same id: ${userId}`)
+                        logger.info(`more than 1 user found with the same id: ${userObject.id}`)
                     }
                 }
                 return context
             }
         ],
         create: [],
-        update: [],
-        patch: [],
+        update: [
+            async function(context) {
+                // logger.info("before update user hook")
+                // logger.info(JSON.stringify(context))
+                if (context.id === "current") {
+                    // logger.info("before update user hook - current")
+                    const userObject = context.params.userObject
+                    const userId = userObject.id
+                    const usersService = context.service
+            
+                    return usersService.patch(userId, { policies: context.data.user.policies })
+                    .then(result => {
+                        // logger.info("after patch current user")
+                        // logger.info(JSON.stringify(result))
+                        context.result = {
+                            email_verified: result.email_verified,
+                            id: "current",
+                            policies: result.policies,
+                            preferred_username: result.preferred_username,
+                        }
+                        return context
+                    })
+                    .catch(error => logger.info(JSON.stringify(error)))
+                } else {
+                    return context
+                }
+            },
+        ],
+        patch: [
+            async function(context) {
+                const usersService = context.service
+                const resultBeforePatch = await usersService.get(context.id)
+                context.policiesBeforeUpdate = resultBeforePatch.users[0].policies
+                return context
+            }
+        ],
         remove: [],
     },
 
     after: {
         all: [],
-        find: [],
-        get: [],
+        find: [
+            function(context) {
+                // logger.info("after find users")
+                // logger.info(JSON.stringify(context))
+                const userResult = context.result.data
+                context.result = {
+                    users: [ ...userResult ],
+                }
+                return context
+            }
+        ],
+        get: [
+            // return what frontend expects to get
+            function(context) {
+                const userResult = context.result
+                userResult.id =  "current"
+                userResult.email_verified = false
+                userResult.links = {
+                    policies: "/users/current/policies",
+                }
+                context.result = {
+                    users: [ userResult ],
+                }
+            },
+        ],
         create: [],
         update: [],
-        patch: [],
-        remove: [],
+        patch: [
+            function(context) {
+                // logger.info("after user patch")
+                // logger.info(JSON.stringify(context))
+                if (!arraysAreEqual(context.policiesBeforeUpdate, context.result.policies)) {
+                    context.app.handleUserPatch(context.id, context.policiesBeforeUpdate, context.result.policies)
+                }
+            }
+        ],
+        remove: [
+            function(context) {
+                // logger.info("after user remove hook")
+                // logger.info(JSON.stringify(context))
+                context.app.handleUserDelete(context.result._id, context.result.policies)
+            }
+        ],
     },
 
     error: {
